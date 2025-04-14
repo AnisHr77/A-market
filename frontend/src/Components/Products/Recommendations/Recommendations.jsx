e4d17c929e55de6dbcf984f5973fb80b85a175dd
Anis Hadj Ramdane <anis28hr@gmail.com>
2025-04-14 21:44:40 +0100

    Recommendations.jsx

--- frontend/src/Components/Products/Recommendations/Recommendations.jsx
+++ frontend/src/Components/Products/Recommendations/Recommendations.jsx
@@ -52,8 +52,8 @@ const Recommendations = () => {
                     disabled={currentIndex === 0}
                     style={{
                         position: 'absolute',
-                        left: '-30px',
-                        top: '27%',
+                         top: '25%',
+                        left:'5px',
                         transform: 'translateY(-50%)',
                         zIndex: 2,
                     }}
@@ -68,6 +68,7 @@ const Recommendations = () => {
                         width: '100%',
                         justifyContent: 'flex-start',
                         paddingBottom: '3rem',
+                        paddingLeft: '2rem',
                     }}
                 >
                     <div
@@ -91,8 +92,8 @@ const Recommendations = () => {
                     disabled={currentIndex >= maxIndex}
                     style={{
                         position: 'absolute',
-                        right: '-20px',
-                        top: '27%',
+                       right: '-50px',
+                        top: '25%',
                         transform: 'translateY(-50%)',
                         zIndex: 2,
                     }}
